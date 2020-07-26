1561913e0236d247158763b940947c7c1b8dcd15
ffreemt <yucongo+fmt@gmail.com>
2020-07-26 16:20:44 +0800

    update tests passed

---
+++ index.js
@@ -0,0 +1,42 @@
+// import consola from "consola";
+const consola = require('consola');
+consola.level = process.env.CONSOLA_DEBUG || 3;
+// consola.level = process.env.CONSOLA_DEBUG || 5; //show debug
+
+const search_es = require("./search_es");
+
+module.exports = fetch_es;
+async function fetch_es(query, index="") {
+  if (!query) {
+    return [];
+  }
+
+  // default indices
+  if (!index) {
+    index = ["yhdcd", "dictcor", "uncor"];
+  }
+
+  let res;
+  try {
+    res = search_es(query, index);
+  } catch (e) {
+    consola.error("search_es e:", e);
+    res = [e.message];
+    return res;
+  }
+
+  // if (res.length)
+  if (res.length > 1)
+    return res;
+
+  try {
+    res = search_es(query, index, "OR"); // search with
+   "OR"
+  } catch (e) {
+    consola.error(`suggest_es e: ${e}`);
+    res = [e.message];
+  }
+
+  return res;
+
+}
---
+++ test/search_es.test.js
@@ -0,0 +1,52 @@
+// const axios_es = require('./axios_es');
+// const expect = require('chai').expect;
+// import "core-js/stable";
+// import "regenerator-runtime/runtime"; // moach -r esm OK
+
+// jest.setTimeout(30000);
+
+// import consola from "consola";
+
+const consola = require("consola");
+consola.level = process.env.CONSOLA_DEBUG || 3;
+// consola.level = process.env.CONSOLA_DEBUG || 5; //show debug
+
+// -r esm
+// import search_es from "../search_es";
+const search_es = require("../search_es");
+
+// import { expect } from "chai";
+expect = require("chai").expect;
+
+describe("@1 search_es: AND returns results ", () => {
+  it("#1 search_es(''): 'empty'", async () => {
+    var result = await search_es("");
+    consola.debug("consola.debug #1", result);
+    expect(result.length).to.equal(0);
+  });
+
+  it("#2 search_es('test') ", async () => {
+    var result = await search_es("test");
+    consola.debug("consola.debug>>>#2:", result);
+    // expect(result.length).to.equal(10);
+    expect(result.length).to.equal(11); // took time
+  });
+
+  let query = "developing countries jointly 国";
+  it("#3 search_es: " + query, async () => {
+    var result = await search_es(query, index="dictcor");
+    consola.debug("#3 consola.debug ", result);
+    // expect(result.length).to.equal(10);
+    expect(result.length).to.equal(0);
+  });
+});
+
+describe("@2 search_es OR", () => {
+  let query = "developing countries jointl 国";
+  it("#1 search_es: " + query, async () => {
+    let result = await search_es(query, "", "OR");
+    consola.debug("#1", result);
+    // expect(result.length).to.equal(0);
+    expect(result.length).to.equal(11);
+  });
+});
